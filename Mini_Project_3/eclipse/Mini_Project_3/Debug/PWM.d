aaf9f6942b2109d7a3404a688868fb9011ed15fe
Ahmed M.Aboelfetouh <74155922+ahmedaboelfetouh@users.noreply.github.com>
2022-04-24 02:03:47 +0200

    Delete Mini_Project_3 directory

--- Mini_Project_3/eclipse/Mini_Project_3/Debug/PWM.d
+++
@@ -1,7 +0,0 @@
-PWM.o PWM.o: ../PWM.c ../PWM.h ../std_types.h ../common_macros.h
-
-../PWM.h:
-
-../std_types.h:
-
-../common_macros.h:
--- Mini_Project_3/eclipse/Mini_Project_3/Debug/lm35_sensor.d
+++
@@ -1,8 +0,0 @@
-lm35_sensor.o lm35_sensor.o: ../lm35_sensor.c ../lm35_sensor.h \
-  ../std_types.h ../adc.h
-
-../lm35_sensor.h:
-
-../std_types.h:
-
-../adc.h:
